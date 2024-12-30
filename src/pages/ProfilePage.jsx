4b01ac9aac01e7978e907e18c3b44b9237d73aa2
Aris Looi <arislooi@outlook.com>
2024-12-30 19:17:38 +0800

    Update IconButton.jsx, ProfileSideBar.jsx, ProfilePage.jsx

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,37 +1,30 @@
-import { Navbar, Container, Button } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useLocalStorage from "use-local-storage";
+import ProfileSideBar from "../components/ProfileSideBar"
 
 export default function ProfilePage() {
     const [authToken, setAuthToken] = useLocalStorage("authToken", "");
     const navigate = useNavigate();
 
+    // Check for authToken immediately upon component mount and whenever authToken changes
     useEffect(() => {
         if (!authToken) {
-            navigate("/login");
+            navigate("/login"); // Redirect to login if no auth token is present
         }
     }, [authToken, navigate]);
 
     const handleLogout = () => {
-        setAuthToken("");
+        setAuthToken(""); // Clear token from localStorage
     };
 
     return (
         <>
-            <Navbar bg="light">
-                <Container>
-                    <Navbar.Brand href="/">
-                        <i className="bi bi-twitter" style={{ fontSize: 30, color: "dodgerblue" }}></i>
-                    </Navbar.Brand>
-                    <Navbar.Collapse className="justify-content-end">
-                        <Button variant="primary" onClick={handleLogout}>Logout</Button>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-
-            <Container className="mt-3">
-                <h2>Your Profile</h2>
+            <Container>
+                <Row>
+                    <ProfileSideBar handleLogout={handleLogout} />
+                </Row>
             </Container>
         </>
     )
