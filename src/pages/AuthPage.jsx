0252057ab811e621699218e46a1dca63d8bb5044
Aris Looi <arislooi@outlook.com>
2024-12-30 10:31:22 +0800

    Add create account button and modal

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,12 @@
-import { Col, Image, Row, Button } from "react-bootstrap";
+import { Col, Image, Row, Button, Modal, Form } from "react-bootstrap";
+import { useState } from "react";
 
 export default function AuthPage() {
     const loginImage = "https://sig1.co/img-twitter-1"
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <Row>
             <Col sm={6}>
@@ -18,8 +23,33 @@ export default function AuthPage() {
                     <Button className="rounded-pill" variant="outline-dark">
                         <i className="bi bi-apple"></i> Sign up with Apple
                     </Button>
+                    <p style={{ textAlign: "center" }}>or</p>
+                    <Button className="rounded-pill" onClick={handleShow}>Create an account</Button>
+                    <p style={{ fontSize: "12px" }}>
+                        By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.
+                    </p>
                 </Col>
+                <Modal show={show} onHide={handleClose} centered>
+                    <Modal.Body className="d-grid gap-2 px-5">
+                        <h2 className="mb-4" style={{ fontWeight: "bold" }}>
+                            Create your account
+                        </h2>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                        </Form>
+                        <p style={{ fontSize: "12px" }}>
+                            By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use. SigmaTweets may use your contact information, including your email address and phone number for purposes outlined in our Privacy Policy, like keeping your account seceure and personalising our services, including ads. Learn more. Others will be able to find you by email or phone number, when provided, unless you choose otherwise here.
+                        </p>
+                        <Button className="rounded-pill">Sign up</Button>
+                    </Modal.Body>
+                </Modal>
             </Col>
+
         </Row>
 
     )
