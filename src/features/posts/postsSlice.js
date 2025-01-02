8f504a45b9ecee9dcfa1c66ab68cad519af75202
Aris Looi <arislooi@outlook.com>
2025-01-02 10:57:07 +0800

    update postsSlice.js

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 const BASE_URL = "https://7e27c269-897c-43f3-828e-b4868ad585c2-00-2r5fr76a35h0r.pike.replit.dev"
 
@@ -11,6 +13,24 @@ export const fetchPostsByUser = createAsyncThunk(
     }
 );
 
+export const savePost = createAsyncThunk(
+    "posts/savePost",
+    async (postContent) => {
+        const token = localStorage.getItem("authToken");
+        const decode = jwtDecode(token);
+        const userId = decode.id;
+
+        const data = {
+            title: "Post Title",
+            content: postContent,
+            user_id: userId,
+        };
+
+        const response = await axios.post(`${BASE_URL}/posts`, data);
+        return response.data;
+    }
+)
+
 const postsSlice = createSlice({
     name: "posts",
     initialState: { posts: [], loading: true },
@@ -19,7 +39,10 @@ const postsSlice = createSlice({
         builder.addCase(fetchPostsByUser.fulfilled, (state, action) => {
             state.posts = action.payload;
             state.loading = false;
-        })
+        }),
+            builder.addCase(savePost.fulfilled, (state, action) => {
+                state.posts = [action.payload, ...state.posts];
+            });
     }
 })
 
