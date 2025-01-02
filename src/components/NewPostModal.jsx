e3efd3550d908aea03ddec92509a2bcd6c5d908a
Aris Looi <arislooi@outlook.com>
2025-01-02 11:26:50 +0800

    Create ChatModal.jsx component

--- src/components/NewPostModal.jsx
+++ src/components/NewPostModal.jsx
@@ -1,38 +1,17 @@
-import axios from "axios";
 import { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
-import { jwtDecode } from "jwt-decode";
+import { useDispatch } from "react-redux";
+import { savePost } from "../features/posts/postsSlice";
 
 export default function NewPostModal({ show, handleClose }) {
     const [postContent, setPostContent] = useState("");
+    const dispatch = useDispatch();
 
     const handleSave = () => {
-        // Get stored JWT Token
-        const token = localStorage.getItem("authToken");
-
-        // Decode the token to fetch user id
-        const decode = jwtDecode(token);
-        const userId = decode.id // May change depending on how the server encode the token
-
-        // Prepare data to be sent
-        const data = {
-            title: "Post Title", //Add functionality to set this properly
-            content: postContent,
-            user_id: userId,
-        };
-
-        // Make you API call here
-        axios
-            .post("https://7e27c269-897c-43f3-828e-b4868ad585c2-00-2r5fr76a35h0r.pike.replit.dev/posts", data)
-            .then((response) => {
-                console.log("Success:", response.data);
-                handleClose();
-            })
-            .catch((error) => {
-                console.error("Error", error);
-            })
-
-    }
+        dispatch(savePost(postContent));
+        handleClose();
+        setPostContent("");
+    };
     return (
         <>
             <Modal show={show} onHide={handleClose}>
